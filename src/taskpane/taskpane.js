5c662ecf1d27966f9d51c0b33287fa6686d83944
RE Hunter <45364073+rehunter145@users.noreply.github.com>
2023-10-16 08:44:00 -0400

    Add files via upload

---
+++ src/taskpane/taskpane.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* global document, Office */
+
+// Office.onReady((info) => {
+//   if (info.host === Office.HostType.Outlook) {
+//     document.getElementById("sideload-msg").style.display = "none";
+//     document.getElementById("app-body").style.display = "flex";
+//     document.getElementById("run").onclick = run;
+//   }
+// });
+
+// export async function run() {
+//   /**
+//    * Insert your Outlook code here
+//    */
+// }
+
+
